58d8c54a8c419d23c9f88157bdaa3d579757cdee
IEatPlates <kaizokuoninarulol@gmail.com>
2026-04-29 15:41:09 +0000

    f

--- src/utils/aniwatch_parser.ts
+++ src/utils/aniwatch_parser.ts
@@ -19,7 +19,7 @@ import type {
   IVoiceActors,
 } from "../types/aniwatch.d.ts";
 
-const BASE_URL: string = "https://aniwatch.com.ro";
+const BASE_URL: string = "https://aniwatch.ca";
 
 const axiosInstance = axios.create({ baseURL: BASE_URL });
 
